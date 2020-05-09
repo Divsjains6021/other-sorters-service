6ddf41143445e911cbec8d51a107cd6d90f77392
Divsjains6021 <divsjains6021@gmail.com>
2020-05-10 00:24:04 +0530

    Property Other Sorters Service

---
+++ src/main/java/com/propertydekho/othersortersservice/OtherSortersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.propertydekho.othersortersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherSortersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtherSortersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/propertydekho/othersortersservice/OtherSortersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propertydekho.othersortersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherSortersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
